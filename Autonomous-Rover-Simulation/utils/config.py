53415df481384b08eedee87ade6e43380e8bb694
Crift3vil <115710498+Crift3vil@users.noreply.github.com>
2026-02-20 06:15:15 +0530

    Delete Autonomous-Rover-Simulation directory

--- Autonomous-Rover-Simulation/utils/config.py
+++
@@ -1,2 +0,0 @@
-ROBOT_RADIUS = 0.5     # meters (50 cm)
-STOP_DISTANCE = 1.0   # safety distance in meters
--- Autonomous-Rover-Simulation/utils/geometry.py
+++
@@ -1,5 +0,0 @@
-import math
-
-
-def normalize_angle(theta):
-    return math.atan2(math.sin(theta), math.cos(theta))
